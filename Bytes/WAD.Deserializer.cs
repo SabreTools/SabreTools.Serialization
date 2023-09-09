7cfe41e22572c7a931a7ac41809e3331c0358236
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 23:16:46 -0400

    Add and convert WAD

---
+++ Bytes/WAD.Deserializer.cs
@@ -0,0 +1,27 @@
+using System.IO;
+
+namespace SabreTools.Serialization.Bytes
+{
+    public partial class WAD : IByteSerializer<Models.WAD.File>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Models.WAD.File Deserialize(byte[] data, int offset)
+#else
+        public Models.WAD.File? Deserialize(byte[]? data, int offset)
+#endif
+        {
+            // If the data is invalid
+            if (data == null)
+                return null;
+
+            // If the offset is out of bounds
+            if (offset < 0 || offset >= data.Length)
+                return null;
+
+            // Create a memory stream and parse that
+            MemoryStream dataStream = new MemoryStream(data, offset, data.Length - offset);
+            return new Streams.WAD().Deserialize(dataStream);
+        }
+    }
+}
