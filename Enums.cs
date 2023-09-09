788a70df3335ad74eaf163d5b59745d6aabb974c
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 22:55:47 -0400

    Add enums and extensions

---
+++ Enums.cs
@@ -0,0 +1,13 @@
+namespace SabreTools.Serialization
+{
+    public enum Hash
+    {
+        CRC,
+        MD5,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+        SpamSum,
+    }
+}
