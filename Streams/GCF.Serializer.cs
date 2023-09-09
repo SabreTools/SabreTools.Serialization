5904756eb80299f86c93750939e6ee1d2734ec80
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 21:58:44 -0400

    Add and convert GCF

---
+++ Streams/GCF.Serializer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+
+namespace SabreTools.Serialization.Streams
+{
+    public partial class GCF : IStreamSerializer<Models.GCF.File>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Stream Serialize(Models.GCF.File obj) => throw new NotImplementedException();
+#else
+        public Stream? Serialize(Models.GCF.File? obj) => throw new NotImplementedException();
+#endif
+    }
+}
