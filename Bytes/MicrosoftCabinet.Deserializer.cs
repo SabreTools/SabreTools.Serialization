b2064cd9b97574ef7ab5e2f139a6f207b3b02b4a
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 22:08:37 -0400

    Add and convert MS-CAB

---
+++ Bytes/MicrosoftCabinet.Deserializer.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using SabreTools.Models.MicrosoftCabinet;
+
+namespace SabreTools.Serialization.Bytes
+{
+    // TODO: Add multi-cabinet reading
+    public partial class MicrosoftCabinet : IByteSerializer<Cabinet>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Cabinet Deserialize(byte[] data, int offset)
+#else
+        public Cabinet? Deserialize(byte[]? data, int offset)
+#endif
+        {
+            // If the data is invalid
+            if (data == null)
+                return null;
+
+            // If the offset is out of bounds
+            if (offset < 0 || offset >= data.Length)
+                return null;
+
+            // Create a memory stream and parse that
+            MemoryStream dataStream = new MemoryStream(data, offset, data.Length - offset);
+            return new Streams.MicrosoftCabinet().Deserialize(dataStream);
+        }
+    }
+}
---
+++ Streams/MicrosoftCabinet.Serializer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using SabreTools.Models.MicrosoftCabinet;
+
+namespace SabreTools.Serialization.Streams
+{
+    public partial class MicrosoftCabinet : IStreamSerializer<Cabinet>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Stream Serialize(Cabinet obj) => throw new NotImplementedException();
+#else
+        public Stream? Serialize(Cabinet? obj) => throw new NotImplementedException();
+#endif
+    }
+}
