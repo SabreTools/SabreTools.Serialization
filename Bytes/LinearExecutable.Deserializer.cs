8b9952d0c701a28d987bee60fc3a17fc0289276f
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 22:41:49 -0400

    Add and convert LE/LX

---
+++ Bytes/LinearExecutable.Deserializer.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using SabreTools.Models.LinearExecutable;
+
+namespace SabreTools.Serialization.Bytes
+{
+    public partial class LinearExecutable : IByteSerializer<Executable>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Executable Deserialize(byte[] data, int offset)
+#else
+        public Executable? Deserialize(byte[]? data, int offset)
+#endif
+        {
+            // If the data is invalid
+            if (data == null)
+                return null;
+
+            // If the offset is out of bounds
+            if (offset < 0 || offset >= data.Length)
+                return null;
+
+            // Create a memory stream and parse that
+            MemoryStream dataStream = new MemoryStream(data, offset, data.Length - offset);
+            return new Streams.LinearExecutable().Deserialize(dataStream);
+        }
+    }
+}
---
+++ Streams/LinearExecutable.Serializer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using SabreTools.Models.LinearExecutable;
+
+namespace SabreTools.Serialization.Streams
+{
+    public partial class LinearExecutable : IStreamSerializer<Executable>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Stream Serialize(Executable obj) => throw new NotImplementedException();
+#else
+        public Stream? Serialize(Executable? obj) => throw new NotImplementedException();
+#endif
+    }
+}
