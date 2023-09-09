43b156a9c3ea9b8b04f0f6d2fe3deb060e96e682
Matt Nadareski <mnadareski@outlook.com>
2023-09-08 22:13:40 -0400

    Add and convert MoPaQ

---
+++ Streams/MoPaQ.Serializer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using SabreTools.Models.MoPaQ;
+
+namespace SabreTools.Serialization.Streams
+{
+    public partial class MoPaQ : IStreamSerializer<Archive>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Stream Serialize(Archive obj) => throw new NotImplementedException();
+#else
+        public Stream? Serialize(Archive? obj) => throw new NotImplementedException();
+#endif
+    }
+}
