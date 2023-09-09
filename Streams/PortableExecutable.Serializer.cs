6a192df1676ed67535748a16f23999e5b545a1e7
Matt Nadareski <mnadareski@outlook.com>
2023-09-09 00:10:17 -0400

    Add and convert PE

---
+++ Streams/PortableExecutable.Serializer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+using SabreTools.Models.PortableExecutable;
+
+namespace SabreTools.Serialization.Streams
+{
+    public partial class PortableExecutable : IStreamSerializer<Executable>
+    {
+        /// <inheritdoc/>
+#if NET48
+        public Stream Serialize(Executable obj) => throw new NotImplementedException();
+#else
+        public Stream? Serialize(Executable? obj) => throw new NotImplementedException();
+#endif
+    }
+}
