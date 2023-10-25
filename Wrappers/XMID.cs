3b90af7b3a3972dcfb22e52b8746ed3fefe2d0f7
Matt Nadareski <mnadareski@outlook.com>
2023-10-25 13:29:59 -0400

    Split XgdInfo into two proper wrappers

---
+++ Wrappers/XMID.cs
@@ -0,0 +1,119 @@
+using System.IO;
+using System.Text;
+using static SabreTools.Models.Xbox.Constants;
+
+namespace SabreTools.Serialization.Wrappers
+{
+    public class XMID : WrapperBase<Models.Xbox.XMID>
+    {
+        #region Descriptive Properties
+
+        /// <inheritdoc/>
+        public override string DescriptionString => "Xbox Media Identifier";
+
+        #endregion
+
+        #region Extension Properties
+
+        /// <summary>
+        /// Get the human-readable publisher string
+        /// </summary>
+        public string Publisher
+        {
+            get
+            {
+                var publisherIdentifier = this.Model.PublisherIdentifier;
+                if (string.IsNullOrWhiteSpace(publisherIdentifier))
+                    return "Unknown";
+
+                if (Publishers.ContainsKey(publisherIdentifier))
+                    return Publishers[publisherIdentifier];
+
+                return $"Unknown ({publisherIdentifier})";
+            }
+        }
+
+        /// <summary>
+        /// Get the human-readable region string
+        /// </summary>
+        public string Region
+        {
+            get
+            {
+                var regionIdentifier = this.Model.RegionIdentifier;
+                if (Regions.ContainsKey(regionIdentifier))
+                    return Regions[regionIdentifier];
+
+                return $"Unknown ({regionIdentifier})";
+            }
+        }
+
+        /// <summary>
+        /// Get the human-readable serial string
+        /// </summary>
+        public string Serial => $"{this.Model.PublisherIdentifier}-{this.Model.GameID}";
+
+        /// <summary>
+        /// Get the human-readable version string
+        /// </summary>
+        public string Version => $"1.{this.Model.VersionNumber}";
+
+        #endregion
+
+        #region Constructors
+
+        /// <inheritdoc/>
+#if NET48
+        public XMID(Models.Xbox.XMID model, byte[] data, int offset)
+#else
+        public XMID(Models.Xbox.XMID? model, byte[]? data, int offset)
+#endif
+            : base(model, data, offset)
+        {
+            // All logic is handled by the base class
+        }
+
+        /// <inheritdoc/>
+#if NET48
+        public XMID(Models.Xbox.XMID model, Stream data)
+#else
+        public XMID(Models.Xbox.XMID? model, Stream? data)
+#endif
+            : base(model, data)
+        {
+            // All logic is handled by the base class
+        }
+
+        /// <summary>
+        /// Create a XMID from a string
+        /// </summary>
+        /// <param name="data">String representing the data</param>
+        /// <returns>A XMID wrapper on success, null on failure</returns>
+#if NET48
+        public static XMID Create(string data)
+#else
+        public static XMID? Create(string? data)
+#endif
+        {
+            // If the data is invalid
+            if (data == null || data.Length == 0)
+                return null;
+
+            var binary = new Strings.XMID().Deserialize(data);
+            if (binary == null)
+                return null;
+
+            try
+            {
+                var ms = new MemoryStream(Encoding.ASCII.GetBytes(data));
+                return new XMID(binary, ms);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Wrappers/XeMID.cs
@@ -0,0 +1,134 @@
+using System.IO;
+using System.Text;
+using static SabreTools.Models.Xbox.Constants;
+
+namespace SabreTools.Serialization.Wrappers
+{
+    public class XeMID : WrapperBase<Models.Xbox.XeMID>
+    {
+        #region Descriptive Properties
+
+        /// <inheritdoc/>
+        public override string DescriptionString => "Xbox 360 Media Identifier";
+
+        #endregion
+
+        #region Extension Properties
+
+        /// <summary>
+        /// Get the human-readable media subtype string
+        /// </summary>
+        public string MediaSubtype
+        {
+            get
+            {
+                char mediaSubtype = this.Model.MediaSubtypeIdentifier;
+                if (MediaSubtypes.ContainsKey(mediaSubtype))
+                    return MediaSubtypes[mediaSubtype];
+
+                return $"Unknown ({mediaSubtype})";
+            }
+        }
+
+        /// <summary>
+        /// Get the human-readable publisher string
+        /// </summary>
+        public string Publisher
+        {
+            get
+            {
+                var publisherIdentifier = this.Model.PublisherIdentifier;
+                if (string.IsNullOrWhiteSpace(publisherIdentifier))
+                    return "Unknown";
+
+                if (Publishers.ContainsKey(publisherIdentifier))
+                    return Publishers[publisherIdentifier];
+
+                return $"Unknown ({publisherIdentifier})";
+            }
+        }
+
+        /// <summary>
+        /// Get the human-readable region string
+        /// </summary>
+        public string Region
+        {
+            get
+            {
+                var regionIdentifier = this.Model.RegionIdentifier;
+                if (Regions.ContainsKey(regionIdentifier))
+                    return Regions[regionIdentifier];
+
+                return $"Unknown ({regionIdentifier})";
+            }
+        }
+
+        /// <summary>
+        /// Get the human-readable serial string
+        /// </summary>
+        public string Serial => $"{this.Model.PublisherIdentifier}-{this.Model.PlatformIdentifier}{this.Model.GameID}";
+
+        /// <summary>
+        /// Get the human-readable version string
+        /// </summary>
+        public string Version => $"1.{this.Model.SKU}";
+
+        #endregion
+
+        #region Constructors
+
+        /// <inheritdoc/>
+#if NET48
+        public XeMID(Models.Xbox.XeMID model, byte[] data, int offset)
+#else
+        public XeMID(Models.Xbox.XeMID? model, byte[]? data, int offset)
+#endif
+            : base(model, data, offset)
+        {
+            // All logic is handled by the base class
+        }
+
+        /// <inheritdoc/>
+#if NET48
+        public XeMID(Models.Xbox.XeMID model, Stream data)
+#else
+        public XeMID(Models.Xbox.XeMID? model, Stream? data)
+#endif
+            : base(model, data)
+        {
+            // All logic is handled by the base class
+        }
+
+        /// <summary>
+        /// Create a XeMID from a string
+        /// </summary>
+        /// <param name="data">String representing the data</param>
+        /// <returns>A XeMID wrapper on success, null on failure</returns>
+#if NET48
+        public static XeMID Create(string data)
+#else
+        public static XeMID? Create(string? data)
+#endif
+        {
+            // If the data is invalid
+            if (data == null || data.Length == 0)
+                return null;
+
+            var binary = new Strings.XeMID().Deserialize(data);
+            if (binary == null)
+                return null;
+
+            try
+            {
+                var ms = new MemoryStream(Encoding.ASCII.GetBytes(data));
+                return new XeMID(binary, ms);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
+}
