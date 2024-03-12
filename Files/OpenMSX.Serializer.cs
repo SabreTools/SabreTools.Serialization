aa538df22910ca140a04a88bf91b06f2891bb35e
Matt Nadareski <mnadareski@outlook.com>
2024-03-11 22:31:40 -0400

    All doctype overloads to take nullable objects

--- Files/OpenMSX.Serializer.cs
+++ Files/OpenMSX.Serializer.cs
@@ -5,7 +5,7 @@ namespace SabreTools.Serialization.Files
     public partial class OpenMSX : XmlFile<SoftwareDb>
     {
         /// <inheritdoc cref="Serialize(SoftwareDb, string, string?, string?, string?, string?)" />
-        public bool SerializeToFileWithDocType(SoftwareDb obj, string path)
+        public bool SerializeToFileWithDocType(SoftwareDb? obj, string path)
             => Serialize(obj, path, Serialization.OpenMSX.DocTypeName, Serialization.OpenMSX.DocTypePubId, Serialization.OpenMSX.DocTypeSysId, Serialization.OpenMSX.DocTypeSysId);
     }
 }
--- Files/SoftwareList.Serializer.cs
+++ Files/SoftwareList.Serializer.cs
@@ -3,7 +3,7 @@ namespace SabreTools.Serialization.Files
     public partial class SoftwareList : XmlFile<Models.SoftwareList.SoftwareList>
     {
         /// <inheritdoc cref="SerializeToFile(Models.SoftwareList.SoftwareList, string, string?, string?, string?, string?)" />
-        public bool SerializeToFileWithDocType(Models.SoftwareList.SoftwareList obj, string path)
+        public bool SerializeToFileWithDocType(Models.SoftwareList.SoftwareList? obj, string path)
             => Serialize(obj, path, Serialization.SoftawreList.DocTypeName, Serialization.SoftawreList.DocTypePubId, Serialization.SoftawreList.DocTypeSysId, Serialization.SoftawreList.DocTypeSysId);
     }
 }
