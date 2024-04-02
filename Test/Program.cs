27d36a11cac63e9a3172bf6238466291a80a78a9
Matt Nadareski <mnadareski@outlook.com>
2024-04-02 11:56:27 -0400

    Add skeleton test executable

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using SabreTools.Serialization;
+
+namespace Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // DO NOTHING FOR NOW
+        }
+    }
+}
