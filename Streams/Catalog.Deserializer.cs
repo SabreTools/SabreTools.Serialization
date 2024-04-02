2b45e8163161f518e4ef36158b145a5ac9fd2443
Deterous <138427222+Deterous@users.noreply.github.com>
2024-04-03 00:44:26 +0900

    typo

--- Streams/Catalog.Deserializer.cs
+++ Streams/Catalog.Deserializer.cs
@@ -4,7 +4,7 @@ using System.Text;
 namespace SabreTools.Serialization.Streams
 {
     public partial class Catalog : JsonFile<Models.Xbox.Catalog>
-
+    {
         // Catalog JSON is encoded as UTF-16 LE
         public new Models.Xbox.Catalog? Deserialize(Stream? data)
             => Deserialize(data, new UnicodeEncoding());
