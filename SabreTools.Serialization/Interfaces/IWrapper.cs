4163b2f22a0987dd1c1859d4b288f4c7046157d4
Matt Nadareski <mnadareski@outlook.com>
2024-04-17 12:28:08 -0400

    Create non-typed variants of IWrapper and WrapperBase

--- SabreTools.Serialization/Interfaces/IWrapper.cs
+++ SabreTools.Serialization/Interfaces/IWrapper.cs
@@ -3,19 +3,13 @@ namespace SabreTools.Serialization.Interfaces
     /// <summary>
     /// Represents a wrapper around a top-level model
     /// </summary>
-    /// <typeparam name="TModel">Top-level model for the wrapper</typeparam>
-    public interface IWrapper<TModel>
+    public interface IWrapper
     {
         /// <summary>
         /// Get a human-readable description of the wrapper
         /// </summary>
         string Description();
 
-        /// <summary>
-        /// Get the backing model
-        /// </summary>
-        TModel GetModel();
-        
 #if !NETFRAMEWORK
         /// <summary>
         /// Export the item information as JSON
