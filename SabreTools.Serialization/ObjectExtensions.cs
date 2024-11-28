ab0e68110c0a54d17e9c6040ca00e0f8d3bf0e69
Matt Nadareski <mnadareski@outlook.com>
2024-11-27 23:43:50 -0500

    Move helper classes out of Wrappers

---
+++ SabreTools.Serialization/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+#if !NETFRAMEWORK
+
+using System;
+
+namespace SabreTools.Serialization
+{
+    /// <summary>
+    /// Extensions for generic object types
+    /// </summary>
+    /// <see href="https://stackoverflow.com/a/72775719"/>
+    internal static class ObjectExtensions
+    {
+        public static T ThrowOnNull<T>(this T? value) where T : class => value ?? throw new ArgumentNullException();
+    }
+}
+
+#endif
