a320ec629e67322bbca33291f0789d564b5473d7
Deterous <138427222+Deterous@users.noreply.github.com>
2024-12-17 13:45:36 +0900

    Add Deserializer for AppPkgHeader

---
+++ SabreTools.Serialization/Deserializers/AppPkgHeader.cs
@@ -0,0 +1,78 @@
+using System.IO;
+using SabreTools.IO.Extensions;
+using static SabreTools.Models.PlayStation4.Constants;
+
+namespace SabreTools.Serialization.Deserializers
+{
+    public class AppPkgHeader : BaseBinaryDeserializer<Models.PlayStation4.AppPkgHeader>
+    {
+        /// <inheritdoc/>
+        public override Models.PlayStation4.AppPkgHeader? Deserialize(Stream? data)
+        {
+            // If the data is invalid
+            if (data == null || !data.CanRead)
+                return null;
+
+            try
+            {
+                // Create a new app.pkg header to fill
+                var appPkgHeader = new Models.PlayStation4.AppPkgHeader();
+
+                appPkgHeader.Magic = data.ReadUInt32BigEndian();
+                if (appPkgHeader.Magic != AppPkgMagic)
+                    return null;
+
+                appPkgHeader.Type = data.ReadUInt32BigEndian();
+                appPkgHeader.PKGUnknown = data.ReadUInt32BigEndian();
+                appPkgHeader.FileCount = data.ReadUInt32BigEndian();
+                appPkgHeader.EntryCount = data.ReadUInt32BigEndian();
+                appPkgHeader.SCEntryCount = data.ReadUInt16BigEndian();
+                appPkgHeader.EntryCount2 = data.ReadUInt16BigEndian();
+                appPkgHeader.TableOffset = data.ReadUInt32BigEndian();
+                appPkgHeader.EntryDataSize = data.ReadUInt32BigEndian();
+                appPkgHeader.BodyOffset = data.ReadUInt64BigEndian();
+                appPkgHeader.BodySize = data.ReadUInt64BigEndian();
+                appPkgHeader.ContentOffset = data.ReadUInt64BigEndian();
+                appPkgHeader.ContentSize = data.ReadUInt64BigEndian();
+                byte[] contentID = data.ReadBytes(0x24);
+                appPkgHeader.ContentID = Encoding.ASCII.GetString(contentID).TrimEnd('\0');
+                appPkgHeader.ContentZeroes = data.ReadBytes(0xC);
+                appPkgHeader.DRMType = data.ReadUInt32BigEndian();
+                appPkgHeader.ContentType = data.ReadUInt32BigEndian();
+                appPkgHeader.ContentFlags = data.ReadUInt32BigEndian();
+                appPkgHeader.PromoteSize = data.ReadUInt32BigEndian();
+                appPkgHeader.VersionDate = data.ReadUInt32BigEndian();
+                appPkgHeader.VersionHash = data.ReadUInt32BigEndian();
+                appPkgHeader.Zeroes1 = data.ReadBytes(0x78);
+                appPkgHeader.MainEntry1SHA256 = data.ReadBytes(0x20);
+                appPkgHeader.MainEntry2SHA256 = data.ReadBytes(0x20);
+                appPkgHeader.DigestTableSHA256 = data.ReadBytes(0x20);
+                appPkgHeader.MainTableSHA256 = data.ReadBytes(0x20);
+                appPkgHeader.Zeroes2 = data.ReadBytes(0x280);
+                appPkgHeader.PSFUnknown = data.ReadUInt32BigEndian();
+                appPkgHeader.PSFImageCount = data.ReadUInt32BigEndian();
+                appPkgHeader.PSFImageFlags = data.ReadUInt64BigEndian();
+                appPkgHeader.PSFImageOffset = data.ReadUInt64BigEndian();
+                appPkgHeader.PSFImageSize = data.ReadUInt64BigEndian();
+                appPkgHeader.MountImageOffset = data.ReadUInt64BigEndian();
+                appPkgHeader.MountImageSize = data.ReadUInt64BigEndian();
+                appPkgHeader.PKGSize = data.ReadUInt64BigEndian();
+                appPkgHeader.PKGSignedSize = data.ReadUInt32BigEndian();
+                appPkgHeader.PKGCacheSize = data.ReadUInt32BigEndian();
+                appPkgHeader.PFSImageSHA256 = data.ReadBytes(0x20);
+                appPkgHeader.PFSSignedSHA256 = data.ReadBytes(0x20);
+                appPkgHeader.PFSSplitSize0 = data.ReadUInt64BigEndian();
+                appPkgHeader.PFSSplitSize1 = data.ReadUInt64BigEndian();
+                appPkgHeader.Zeroes3 = data.ReadBytes(0xB50);
+                appPkgHeader.PKGSHA256 = data.ReadBytes(0x20);
+
+                return header;
+            }
+            catch
+            {
+                // Ignore the actual error
+                return null;
+            }
+        }
+    }
+}
