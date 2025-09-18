6d01e145beca9afbd8dcb457945062c4b4303750
HeroponRikiBestest <jojloj3@gmail.com>
2025-09-18 14:09:56 -0400

    Seperate deserializer into helper methods

--- SabreTools.Serialization/Deserializers/SecuROMMatroschkaPackage.cs
+++ SabreTools.Serialization/Deserializers/SecuROMMatroschkaPackage.cs
@@ -39,56 +39,73 @@ namespace SabreTools.Serialization.Deserializers
 
             try
             {
-                // Create a new file to fill
                 // TODO: Unify matroschka spelling. They spell it matroschka in all official stuff, as far as has been observed. Will double check.
-                var matroschka = new MatroshkaPackage();
-
-                #region Header
-
                 // Try to parse the header
-                byte[] magic = data.ReadBytes(4);
-                matroschka.Signature = Encoding.ASCII.GetString(magic);
-                if (matroschka.Signature != MatroshkaMagicString)
+                if (!ParsePreEntryHeader(data, out MatroshkaPackage matroschka))
                     return null;
-
-                matroschka.EntryCount = data.ReadUInt32LittleEndian();
-                if (matroschka.EntryCount == 0)
-                    return null; // TODO: This should never occur, log output should happen even without debug.
                 
-                // Check if "matrosch" section is a longer header one or not based on whether the next uint is 0 or 1. Anything
-                // else will just already be starting the filename string, which is never going to start with this.
-                // Previously thought that the longer header was correlated with RC, but at least one executable
-                // (NecroVisioN.exe from the GamersGate patch NecroVisioN_Patch1.2_GG.exe) isn't RC and still has it.
-                long tempPosition = data.Position;
-                uint tempValue = data.ReadUInt32LittleEndian();
-                data.Position = tempPosition;
+                if (!ParseEntries(data, matroschka, out MatroshkaEntry[] entries))
+                    return null;
                 
-                MatroschkaHeaderType matHeaderType;
-                if (tempValue < 2) // Only big-endian 0 or 1 have been observed for long sections.
-                {
-                    matHeaderType = MatroschkaHeaderType.LongHeader;
-                    matroschka.UnknownRCValue1 = data.ReadUInt32LittleEndian();
-                    matroschka.UnknownRCValue2 = data.ReadUInt32LittleEndian();
-                    matroschka.UnknownRCValue3 = data.ReadUInt32LittleEndian();
+                matroschka.Entries = entries;
+                return matroschka;
+            }
+            catch
+            {
+                // Ignore the actual error
+                return null;
+            }
+        }
 
-                    // TODO: Not actually reliable for distinguishing keys, update models documentation to reflect.
-                    // Exact byte count has to be used because non-RC executables have all 0x00 here.
-                    matroschka.KeyHexString = Encoding.ASCII.GetString(data.ReadBytes(32));
-                    if (!data.ReadBytes(4).EqualsExactly([0x00, 0x00, 0x00, 0x00]))
-                        return null; // TODO: This should never occur, log output should happen even without debug.
-                }
-                else
-                    matHeaderType = MatroschkaHeaderType.ShortHeader;
+        public bool ParsePreEntryHeader(Stream data, out MatroshkaPackage matroschka)
+        { 
+            matroschka = new MatroshkaPackage();
+            byte[] magic = data.ReadBytes(4);
+            matroschka.Signature = Encoding.ASCII.GetString(magic);
+            if (matroschka.Signature != MatroshkaMagicString)
+                return false;
 
-                if (matHeaderType > MatroschkaHeaderType.LongHeader)
-                    return null; // TODO: Only here as a placeholder before this value gets used elsewhere to avoid compiler error.
+            matroschka.EntryCount = data.ReadUInt32LittleEndian();
+            if (matroschka.EntryCount == 0)
+                return false; // TODO: This should never occur, log output should happen even without debug.
 
-                #endregion
+            // Check if "matrosch" section is a longer header one or not based on whether the next uint is 0 or 1. Anything
+            // else will just already be starting the filename string, which is never going to start with this.
+            // Previously thought that the longer header was correlated with RC, but at least one executable
+            // (NecroVisioN.exe from the GamersGate patch NecroVisioN_Patch1.2_GG.exe) isn't RC and still has it.
+            long tempPosition = data.Position;
+            uint tempValue = data.ReadUInt32LittleEndian();
+            data.Position = tempPosition;
 
-                #region Entries
+            MatroschkaHeaderType matHeaderType;
+            if (tempValue < 2) // Only big-endian 0 or 1 have been observed for long sections.
+            {
+                matHeaderType = MatroschkaHeaderType.LongHeader;
+                matroschka.UnknownRCValue1 = data.ReadUInt32LittleEndian();
+                matroschka.UnknownRCValue2 = data.ReadUInt32LittleEndian();
+                matroschka.UnknownRCValue3 = data.ReadUInt32LittleEndian();
 
+                // TODO: Not actually reliable for distinguishing keys, update models documentation to reflect.
+                // Exact byte count has to be used because non-RC executables have all 0x00 here.
+                matroschka.KeyHexString = Encoding.ASCII.GetString(data.ReadBytes(32));
+                if (!data.ReadBytes(4).EqualsExactly([0x00, 0x00, 0x00, 0x00]))
+                    return false; // TODO: This should never occur, log output should happen even without debug.
+            }
+            else
+                matHeaderType = MatroschkaHeaderType.ShortHeader;
+
+            if (matHeaderType > MatroschkaHeaderType.LongHeader)
+                return false; // TODO: Only here as a placeholder before this value gets used elsewhere to avoid compiler error.
+
+            return true;
+        }
+
+        public bool ParseEntries(Stream data, MatroshkaPackage matroschka, out MatroshkaEntry[] entries)
+        {
+                long tempPosition;
+                
                 // If we have any entries
-                MatroshkaEntry[] entries = new MatroshkaEntry[matroschka.EntryCount];
+                entries = new MatroshkaEntry[matroschka.EntryCount];
 
                 MatroschkaGapType matGapType = MatroschkaGapType.Error;
                 MatroschkaHasUnknown matHasUnknown = MatroschkaHasUnknown.Error;
@@ -104,31 +121,18 @@ namespace SabreTools.Serialization.Deserializers
                     if (pathString != null)
                         entry.Path = Encoding.ASCII.GetBytes(pathString);
                     else
-                        return null; // TODO: This should never occur, log output should happen even without debug.
+                        return false; // TODO: This should never occur, log output should happen even without debug.
                     data.Position = tempPosition;
                     
                     // Determine if gap size is 256 or 512 bytes, then jump the gap.
                     if (matGapType == MatroschkaGapType.Error)
-                    {
-                        data.Position = tempPosition + 256;
-                        tempPosition = data.Position;
-                        tempValue = data.ReadUInt32LittleEndian();
-                        data.Position = tempPosition;
-                        if (tempValue <= 0) // Gap is 512 bytes. Actually just == 0, but ST prefers ranges.
-                        {
-                            matGapType = MatroschkaGapType.LongGap;
-                            data.Position += 256;
-                        }
-                        else // Gap is 256 bytes.
-                            matGapType = MatroschkaGapType.ShortGap;
-                        
-                    } // If already known, simply jump the gap
-                    else if (matGapType == MatroschkaGapType.ShortGap)
+                        matGapType = GapHelper(data, tempPosition);
+                    else if (matGapType == MatroschkaGapType.ShortGap) // If already known, simply jump the gap
                         data.Position += 256;
                     else if (matGapType == MatroschkaGapType.LongGap)
                         data.Position += 512;
                     else
-                        return null; // TODO: This should never occur, log output should happen even without debug.
+                        return false; // TODO: This should never occur, log output should happen even without debug.
                     
                     // Entry type isn't currently validated as it's always predictable anyways, nor necessary to know.
                     entry.EntryType = (MatroshkaEntryType)data.ReadUInt32LittleEndian();
@@ -137,25 +141,11 @@ namespace SabreTools.Serialization.Deserializers
                     
                     // Check for unknown 4-byte 0x00 value. Not correlated with 256 vs 512-byte gaps.
                     if (matHasUnknown == MatroschkaHasUnknown.Error)
-                    {
-                        tempPosition = data.Position;
-                        tempValue = data.ReadUInt32LittleEndian();
-                        if (tempValue <= 0) // Entry has the Unknown value.
-                        {
-                            matHasUnknown = MatroschkaHasUnknown.HasUnknown;
-                            entry.Unknown = tempValue;
-                        } 
-                        else
-                        {
-                            matHasUnknown = MatroschkaHasUnknown.NoUnknown;
-                            data.Position = tempPosition;
-                        }
-                        
-                    } // If already known, read or don't read the unknown value.
-                    else if (matHasUnknown == MatroschkaHasUnknown.HasUnknown)
+                        matHasUnknown = UnknownHelper(data, entry);
+                    else if (matHasUnknown == MatroschkaHasUnknown.HasUnknown) // If already known, read or don't read the unknown value.
                         entry.Unknown = data.ReadUInt32LittleEndian(); // TODO: Validate it's zero?
                     else if (matHasUnknown != MatroschkaHasUnknown.NoUnknown)
-                        return null; // TODO: This should never occur, log output should happen even without debug.
+                        return false; // TODO: This should never occur, log output should happen even without debug.
                     
                     entry.ModifiedTime = data.ReadUInt64LittleEndian();
                     entry.CreatedTime = data.ReadUInt64LittleEndian();
@@ -163,18 +153,46 @@ namespace SabreTools.Serialization.Deserializers
                     entry.MD5 = data.ReadBytes(16);
                     entries[i] = entry;
                 }
-                
-                matroschka.Entries = entries;
-                
-                #endregion
 
-                return matroschka;
-            }
-            catch
+                return true;
+        }
+
+        public MatroschkaGapType GapHelper(Stream data, long tempPosition)
+        {
+            uint tempValue;
+            data.Position = tempPosition + 256;
+            tempPosition = data.Position;
+            tempValue = data.ReadUInt32LittleEndian();
+            data.Position = tempPosition;
+            MatroschkaGapType matGapType;
+            if (tempValue <= 0) // Gap is 512 bytes. Actually just == 0, but ST prefers ranges.
             {
-                // Ignore the actual error
-                return null;
+                matGapType = MatroschkaGapType.LongGap;
+                data.Position += 256;
             }
+            else // Gap is 256 bytes.
+                matGapType = MatroschkaGapType.ShortGap;
+            
+            return matGapType;
+        }
+        
+        public MatroschkaHasUnknown UnknownHelper(Stream data, MatroshkaEntry entry)
+        {
+            long tempPosition = data.Position;
+            uint tempValue = data.ReadUInt32LittleEndian();
+            MatroschkaHasUnknown matHasUnknown;
+            if (tempValue <= 0) // Entry has the Unknown value.
+            {
+                matHasUnknown = MatroschkaHasUnknown.HasUnknown;
+                entry.Unknown = tempValue; // TODO: Will entry be changed outside of this function?
+            } 
+            else
+            {
+                matHasUnknown = MatroschkaHasUnknown.NoUnknown;
+                data.Position = tempPosition;
+            }
+            
+            return matHasUnknown;
         }
     }
 }
