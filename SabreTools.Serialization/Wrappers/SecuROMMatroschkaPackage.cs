c2d3a071884259363d64526fd5193ce5c3c2de13
HeroponRikiBestest <jojloj3@gmail.com>
2025-09-18 09:37:18 -0400

    Fix GA building; thank you sabre

--- SabreTools.Serialization/Wrappers/SecuROMMatroschkaPackage.cs
+++ SabreTools.Serialization/Wrappers/SecuROMMatroschkaPackage.cs
@@ -95,7 +95,7 @@ namespace SabreTools.Serialization.Wrappers
                 // Cache the current offset
                 long currentOffset = data.Position;
 
-                var model = Deserializers.SecuROMMatroschkaPackage.DeserializeStream(data);
+                var model = new Deserializers.SecuROMMatroschkaPackage().Deserialize(data);
                 if (model == null)
                     return null;
 
