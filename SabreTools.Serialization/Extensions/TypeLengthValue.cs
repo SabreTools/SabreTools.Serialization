b97dbc2ac5914c45621809c8743fc84ddf6fb1d1
Matt Nadareski <mnadareski@outlook.com>
2025-09-24 09:22:23 -0400

    Simplify extension code slightly

--- SabreTools.Serialization/Extensions/TypeLengthValue.cs
+++ SabreTools.Serialization/Extensions/TypeLengthValue.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Numerics;
 using System.Text;
+using SabreTools.Serialization.ASN1;
 
 namespace SabreTools.Serialization.Extensions
 {
@@ -21,7 +22,7 @@ namespace SabreTools.Serialization.Extensions
 
             // Append the type
             formatBuilder.Append($"{padding}Type: {tlv.Type}");
-            if (tlv.Type == ASN1.ASN1Type.V_ASN1_EOC)
+            if (tlv.Type == ASN1Type.V_ASN1_EOC)
                 return formatBuilder.ToString();
 
             // Append the length
@@ -31,9 +32,9 @@ namespace SabreTools.Serialization.Extensions
 
             // If we have a constructed type
 #if NET20 || NET35
-            if ((tlv.Type & ASN1.ASN1Type.V_ASN1_CONSTRUCTED) != 0)
+            if ((tlv.Type & ASN1Type.V_ASN1_CONSTRUCTED) != 0)
 #else
-            if (tlv.Type.HasFlag(ASN1.ASN1Type.V_ASN1_CONSTRUCTED))
+            if (tlv.Type.HasFlag(ASN1Type.V_ASN1_CONSTRUCTED))
 #endif
             {
                 if (tlv.Value is not ASN1.TypeLengthValue[] valueAsObjectArray)
@@ -69,7 +70,7 @@ namespace SabreTools.Serialization.Extensions
             switch (tlv.Type)
             {
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-boolean"/>
-                case ASN1.ASN1Type.V_ASN1_BOOLEAN:
+                case ASN1Type.V_ASN1_BOOLEAN:
                     if (tlv.Length > 1)
                         formatBuilder.Append($" [Expected length of 1]");
                     else if (valueAsByteArray.Length > 1)
@@ -80,14 +81,14 @@ namespace SabreTools.Serialization.Extensions
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-integer"/>
-                case ASN1.ASN1Type.V_ASN1_INTEGER:
+                case ASN1Type.V_ASN1_INTEGER:
                     Array.Reverse(valueAsByteArray);
                     var integerValue = new BigInteger(valueAsByteArray);
                     formatBuilder.Append($", Value: {integerValue}");
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-bit-string"/>
-                case ASN1.ASN1Type.V_ASN1_BIT_STRING:
+                case ASN1Type.V_ASN1_BIT_STRING:
                     // TODO: Read into a BitArray and print that out instead?
                     int unusedBits = valueAsByteArray[0];
                     if (unusedBits == 0)
@@ -97,43 +98,43 @@ namespace SabreTools.Serialization.Extensions
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-octet-string"/>
-                case ASN1.ASN1Type.V_ASN1_OCTET_STRING:
+                case ASN1Type.V_ASN1_OCTET_STRING:
                     formatBuilder.Append($", Value: {BitConverter.ToString(valueAsByteArray).Replace('-', ' ')}");
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-object-identifier"/>
                 /// <see cref="http://snmpsharpnet.com/index.php/2009/03/02/ber-encoding-and-decoding-oid-values/"/>
-                case ASN1.ASN1Type.V_ASN1_OBJECT:
+                case ASN1Type.V_ASN1_OBJECT:
                     // Derive array of values
-                    ulong[] objectNodes = ASN1.ObjectIdentifier.ParseDERIntoArray(valueAsByteArray, tlv.Length);
+                    ulong[] objectNodes = ObjectIdentifier.ParseDERIntoArray(valueAsByteArray, tlv.Length);
 
                     // Append the dot and modified OID-IRI notations
-                    string? dotNotationString = ASN1.ObjectIdentifier.ParseOIDToDotNotation(objectNodes);
-                    string? oidIriString = ASN1.ObjectIdentifier.ParseOIDToOIDIRINotation(objectNodes);
+                    string? dotNotationString = ObjectIdentifier.ParseOIDToDotNotation(objectNodes);
+                    string? oidIriString = ObjectIdentifier.ParseOIDToOIDIRINotation(objectNodes);
                     formatBuilder.Append($", Value: {dotNotationString} ({oidIriString})");
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-utf8string"/>
-                case ASN1.ASN1Type.V_ASN1_UTF8STRING:
+                case ASN1Type.V_ASN1_UTF8STRING:
                     formatBuilder.Append($", Value: {Encoding.UTF8.GetString(valueAsByteArray)}");
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-printablestring"/>
-                case ASN1.ASN1Type.V_ASN1_PRINTABLESTRING:
+                case ASN1Type.V_ASN1_PRINTABLESTRING:
                     formatBuilder.Append($", Value: {Encoding.ASCII.GetString(valueAsByteArray)}");
                     break;
 
                 //case ASN1Type.V_ASN1_T61STRING:
-                case ASN1.ASN1Type.V_ASN1_TELETEXSTRING:
+                case ASN1Type.V_ASN1_TELETEXSTRING:
                     formatBuilder.Append($", Value: {Encoding.ASCII.GetString(valueAsByteArray)}");
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-ia5string"/>
-                case ASN1.ASN1Type.V_ASN1_IA5STRING:
+                case ASN1Type.V_ASN1_IA5STRING:
                     formatBuilder.Append($", Value: {Encoding.ASCII.GetString(valueAsByteArray)}");
                     break;
 
-                case ASN1.ASN1Type.V_ASN1_UTCTIME:
+                case ASN1Type.V_ASN1_UTCTIME:
                     string utctimeString = Encoding.ASCII.GetString(valueAsByteArray);
                     if (DateTime.TryParse(utctimeString, out DateTime utctimeDateTime))
                         formatBuilder.Append($", Value: {utctimeDateTime:yyyy-MM-dd HH:mm:ss}");
@@ -142,7 +143,7 @@ namespace SabreTools.Serialization.Extensions
                     break;
 
                 /// <see href="https://learn.microsoft.com/en-us/windows/win32/seccertenroll/about-bmpstring"/>
-                case ASN1.ASN1Type.V_ASN1_BMPSTRING:
+                case ASN1Type.V_ASN1_BMPSTRING:
                     formatBuilder.Append($", Value: {Encoding.Unicode.GetString(valueAsByteArray)}");
                     break;
 
