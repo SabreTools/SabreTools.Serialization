350924545cd109c35e10a073af71361e936d3a3b
Matt Nadareski <mnadareski@outlook.com>
2025-09-26 23:49:35 -0400

    Fix bzip constants

--- SabreTools.Serialization/Models/BZip2/Constants.cs
+++ SabreTools.Serialization/Models/BZip2/Constants.cs
@@ -2,8 +2,8 @@ namespace SabreTools.Data.Models.BZip2
 {
     public static class Constants
     {
-        public static readonly byte[] SignatureBytes = [0x42, 0x52, 0x68];
+        public static readonly byte[] SignatureBytes = [0x42, 0x5A, 0x68];
 
-        public const string SignatureString = "BRh";
+        public const string SignatureString = "BZh";
     }
 }
