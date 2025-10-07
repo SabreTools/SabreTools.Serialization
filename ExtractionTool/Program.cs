aaa8bbe70954ccb4e1bdc44a9175d4e48a2d2255
Matt Nadareski <mnadareski@outlook.com>
2025-10-07 11:25:35 -0400

    This was inconsistent too

--- ExtractionTool/Program.cs
+++ ExtractionTool/Program.cs
@@ -6,7 +6,7 @@ using SabreTools.CommandLine.Features;
 
 namespace ExtractionTool
 {
-    class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
