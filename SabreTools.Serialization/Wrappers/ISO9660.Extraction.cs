c22dcc29904e07c63f63e290a889730348ef0959
Deterous <138427222+Deterous@users.noreply.github.com>
2025-10-26 17:27:44 +0900

    extractable ISO

---
+++ SabreTools.Serialization/Wrappers/ISO9660.Extraction.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using SabreTools.Data.Models.ISO9660;
+
+namespace SabreTools.Serialization.Wrappers
+{
+    public partial class ISO9660 : IExtractable
+    {
+        /// <inheritdoc/>
+        public bool Extract(string outputDirectory, bool includeDebug)
+        {
+            // If we have no root directory
+            if (RootDirectoryExtent == null)
+                return false;
+
+            bool allExtracted = true;
+
+            // TODO: Extract all directories and file extents
+
+            return allExtracted;
+        }
+    }
+}
