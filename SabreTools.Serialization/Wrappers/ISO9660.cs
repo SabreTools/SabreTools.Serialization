8f48a8b4333a35c3e9bef39446e8f7a562ebdad2
Deterous <138427222+Deterous@users.noreply.github.com>
2025-10-29 01:24:08 +0900

    full namespace

--- SabreTools.Serialization/Wrappers/ISO9660.cs
+++ SabreTools.Serialization/Wrappers/ISO9660.cs
@@ -29,7 +29,7 @@ namespace SabreTools.Serialization.Wrappers
         /// <summary>
         /// Directory Descriptors
         /// </summary>
-        public Dictionary<int, ISO9660.Directory> DirectoryDescriptors => Model.DirectoryDescriptors ?? [];
+        public Dictionary<int, SabreTools.Data.Models.ISO9660.Directory> DirectoryDescriptors => Model.DirectoryDescriptors ?? [];
 
         #endregion
 
