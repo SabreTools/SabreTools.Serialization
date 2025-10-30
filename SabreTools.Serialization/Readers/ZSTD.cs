82c436451340a5d8f3a93a80e962d25651dd46ea
HeroponRikiBestest <jojloj3@gmail.com>
2025-10-29 21:46:52 -0400

    Move section

--- SabreTools.Serialization/Readers/ZSTD.cs
+++ SabreTools.Serialization/Readers/ZSTD.cs
@@ -25,6 +25,12 @@ namespace SabreTools.Serialization.Readers
                 if (header == null)
                     return null;
                 
+                // Valid versions are 0x1E and 0x22-0x28.
+                // According to https://datatracker.ietf.org/doc/html/rfc8878#section-7.1-2.22.2.4, the current version is
+                // still 0x28, and it should stay that way now that it's a stable format.
+                if ((header.VersionByte < 0x22 || header.VersionByte > 0x28) && header.VersionByte != 0x1E)
+                    return null;
+                
                 #endregion
 
                 return header;
@@ -48,12 +54,6 @@ namespace SabreTools.Serialization.Readers
             obj.VersionByte = data.ReadByte(); // ReadByte returning int is actually preferable here
             obj.Magic = data.ReadBytes(3);
             if (!obj.Magic.EqualsExactly(SignatureBytes))
-                return null;
-
-            // Valid versions are 0x1E and 0x22-0x28.
-            // According to https://datatracker.ietf.org/doc/html/rfc8878#section-7.1-2.22.2.4, the current version is
-            // still 0x28, and it should stay that way now that it's a stable format.
-            if (obj.VersionByte is < 0x22 or > 0x28 && obj.VersionByte != 0x1E)
                 return null;
  
             return obj;
