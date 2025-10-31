abd8ef3b1f5ed6e800dad1c41476a652feda716d
Matt Nadareski <mnadareski@outlook.com>
2025-10-30 23:55:08 -0400

    Xbox model cleanup

--- SabreTools.Serialization/Models/Xbox/XeMID.cs
+++ SabreTools.Serialization/Models/Xbox/XeMID.cs
@@ -22,7 +22,7 @@ namespace SabreTools.Data.Models.Xbox
         /// <summary>
         /// 2-character publisher identifier
         /// </summary>
-        public string? PublisherIdentifier { get; set; }
+        public string PublisherIdentifier { get; set; }
 
         /// <summary>
         /// 1-character Platform disc is made for, 2 indicates Xbox 360
@@ -32,12 +32,12 @@ namespace SabreTools.Data.Models.Xbox
         /// <summary>
         /// 3-character Game ID
         /// </summary>
-        public string? GameID { get; set; }
+        public string GameID { get; set; }
 
         /// <summary>
         /// 2-character Title-specific SKU
         /// </summary>
-        public string? SKU { get; set; }
+        public string SKU { get; set; }
 
         /// <summary>
         /// 1-character Region identifier character
@@ -47,7 +47,7 @@ namespace SabreTools.Data.Models.Xbox
         /// <summary>
         /// 2-character Base version of executables, usually starts at 01
         /// </summary>
-        public string? BaseVersion { get; set; }
+        public string BaseVersion { get; set; }
 
         /// <summary>
         /// 1-character Media subtype identifier
@@ -57,11 +57,11 @@ namespace SabreTools.Data.Models.Xbox
         /// <summary>
         /// 2-character Disc number stored in [disc number][total discs] format
         /// </summary>
-        public string? DiscNumberIdentifier { get; set; }
+        public string DiscNumberIdentifier { get; set; }
 
         /// <summary>
         /// 8-hex-digit certification submission identifier; usually on test discs only
         /// </summary>
-        public string? CertificationSubmissionIdentifier { get; set; }
+        public string CertificationSubmissionIdentifier { get; set; }
     }
 }
