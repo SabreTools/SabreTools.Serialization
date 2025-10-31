96bc301e6dbc0c2cd10535d1cb6e72d1642207f1
Matt Nadareski <mnadareski@outlook.com>
2025-10-30 21:38:17 -0400

    GCF model cleanup

--- SabreTools.Serialization/Models/GCF/File.cs
+++ SabreTools.Serialization/Models/GCF/File.cs
@@ -11,108 +11,108 @@ namespace SabreTools.Data.Models.GCF
         /// <summary>
         /// Header data
         /// </summary>
-        public Header? Header { get; set; }
+        public Header Header { get; set; }
 
         /// <summary>
         /// Block entry header data
         /// </summary>
-        public BlockEntryHeader? BlockEntryHeader { get; set; }
+        public BlockEntryHeader BlockEntryHeader { get; set; }
 
         /// <summary>
         /// Block entries data
         /// </summary>
-        public BlockEntry[]? BlockEntries { get; set; }
+        public BlockEntry[] BlockEntries { get; set; }
 
         /// <summary>
         /// Fragmentation map header data
         /// </summary>
-        public FragmentationMapHeader? FragmentationMapHeader { get; set; }
+        public FragmentationMapHeader FragmentationMapHeader { get; set; }
 
         /// <summary>
         /// Fragmentation map data
         /// </summary>
-        public FragmentationMap[]? FragmentationMaps { get; set; }
+        public FragmentationMap[] FragmentationMaps { get; set; }
 
         /// <summary>
         /// Block entry map header data
         /// </summary>
         /// <remarks>Part of version 5 but not version 6.</remarks>
-        public BlockEntryMapHeader? BlockEntryMapHeader { get; set; }
+        public BlockEntryMapHeader BlockEntryMapHeader { get; set; }
 
         /// <summary>
         /// Block entry map data
         /// </summary>
         /// <remarks>Part of version 5 but not version 6.</remarks>
-        public BlockEntryMap[]? BlockEntryMaps { get; set; }
+        public BlockEntryMap[] BlockEntryMaps { get; set; }
 
         /// <summary>
         /// Directory header data
         /// </summary>
-        public DirectoryHeader? DirectoryHeader { get; set; }
+        public DirectoryHeader DirectoryHeader { get; set; }
 
         /// <summary>
         /// Directory entries data
         /// </summary>
-        public DirectoryEntry[]? DirectoryEntries { get; set; }
+        public DirectoryEntry[] DirectoryEntries { get; set; }
 
         /// <summary>
         /// Directory names data
         /// </summary>
-        public Dictionary<long, string?>? DirectoryNames { get; set; }
+        public Dictionary<long, string?> DirectoryNames { get; set; }
 
         /// <summary>
         /// Directory info 1 entries data
         /// </summary>
-        public DirectoryInfo1Entry[]? DirectoryInfo1Entries { get; set; }
+        public DirectoryInfo1Entry[] DirectoryInfo1Entries { get; set; }
 
         /// <summary>
         /// Directory info 2 entries data
         /// </summary>
-        public DirectoryInfo2Entry[]? DirectoryInfo2Entries { get; set; }
+        public DirectoryInfo2Entry[] DirectoryInfo2Entries { get; set; }
 
         /// <summary>
         /// Directory copy entries data
         /// </summary>
-        public DirectoryCopyEntry[]? DirectoryCopyEntries { get; set; }
+        public DirectoryCopyEntry[] DirectoryCopyEntries { get; set; }
 
         /// <summary>
         /// Directory local entries data
         /// </summary>
-        public DirectoryLocalEntry[]? DirectoryLocalEntries { get; set; }
+        public DirectoryLocalEntry[] DirectoryLocalEntries { get; set; }
 
         /// <summary>
         /// Directory map header data
         /// </summary>
-        public DirectoryMapHeader? DirectoryMapHeader { get; set; }
+        public DirectoryMapHeader DirectoryMapHeader { get; set; }
 
         /// <summary>
         /// Directory map entries data
         /// </summary>
-        public DirectoryMapEntry[]? DirectoryMapEntries { get; set; }
+        public DirectoryMapEntry[] DirectoryMapEntries { get; set; }
 
         /// <summary>
         /// Checksum header data
         /// </summary>
-        public ChecksumHeader? ChecksumHeader { get; set; }
+        public ChecksumHeader ChecksumHeader { get; set; }
 
         /// <summary>
         /// Checksum map header data
         /// </summary>
-        public ChecksumMapHeader? ChecksumMapHeader { get; set; }
+        public ChecksumMapHeader ChecksumMapHeader { get; set; }
 
         /// <summary>
         /// Checksum map entries data
         /// </summary>
-        public ChecksumMapEntry[]? ChecksumMapEntries { get; set; }
+        public ChecksumMapEntry[] ChecksumMapEntries { get; set; }
 
         /// <summary>
         /// Checksum entries data
         /// </summary>
-        public ChecksumEntry[]? ChecksumEntries { get; set; }
+        public ChecksumEntry[] ChecksumEntries { get; set; }
 
         /// <summary>
         /// Data block header data
         /// </summary>
-        public DataBlockHeader? DataBlockHeader { get; set; }
+        public DataBlockHeader DataBlockHeader { get; set; }
     }
 }
