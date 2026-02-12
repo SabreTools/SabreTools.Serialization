c99f86ee9536b5993421d2c68cb7725757d7ddb1
Matt Nadareski <mnadareski@outlook.com>
2026-02-12 16:55:27 -0500

    Add unused generic resource entry

---
+++ SabreTools.Serialization/Models/PortableExecutable/Resource/Entries/GenericResourceEntry.cs
@@ -0,0 +1,13 @@
+namespace SabreTools.Data.Models.PortableExecutable.Resource.Entries
+{
+    /// <summary>
+    /// Generic or unparsed resource data
+    /// </summary>
+    public abstract class GenericResourceEntry : ResourceDataType
+    {
+        /// <summary>
+        /// Unparsed byte data from the resource
+        /// </summary>
+        public byte[] Data { get; set; } = [];
+    }
+}
