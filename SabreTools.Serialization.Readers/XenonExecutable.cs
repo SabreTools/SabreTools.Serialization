591cea30b629e236460978db193e3c37df3bbf7c
Deterous <138427222+Deterous@users.noreply.github.com>
2026-04-02 13:56:01 +0900

    Update reader

--- SabreTools.Serialization.Readers/XenonExecutable.cs
+++ SabreTools.Serialization.Readers/XenonExecutable.cs
@@ -58,15 +58,17 @@ namespace SabreTools.Serialization.Readers
             obj.ModuleFlags = data.ReadUInt32BigEndian();
             obj.PEDataOffset = data.ReadUInt32BigEndian();
             obj.Reserved = data.ReadUInt32BigEndian();
-            obj.SecurityInfoOffset = data.ReadUInt32BigEndian();
+            obj.CertificateOffset = data.ReadUInt32BigEndian();
             obj.OptionalHeaderCount = data.ReadUInt32BigEndian();
 
             var optionalHeaders = new OptionalHeader[obj.OptionalHeaderCount];
             for (int i = 0; i < obj.OptionalHeaderCount; i++)
             {
-                optionalHeaders[i] = new OptionalHeader();
-                optionalHeaders[i].HeaderID = data.ReadUInt32BigEndian();
-                optionalHeaders[i].HeaderData = data.ReadUInt32BigEndian();
+                var optionalHeader = new OptionalHeader();
+                optionalHeader.HeaderID = data.ReadUInt32BigEndian();
+                optionalHeader.HeaderData = data.ReadUInt32BigEndian();
+
+                optionalHeaders[i] = optionalHeader;
             }
 
             obj.OptionalHeaders = optionalHeaders;
