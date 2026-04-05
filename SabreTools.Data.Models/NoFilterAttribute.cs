285f3a267e60c572a9b73f30b442171223ce35dd
Matt Nadareski <mnadareski@outlook.com>
2026-04-05 00:26:23 -0400

    NoFilter attribute no longer matters

--- SabreTools.Data.Models/NoFilterAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace SabreTools.Data.Models
-{
-    /// <summary>
-    /// Marks a key as unable to be filtered on
-    /// </summary>
-    [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class NoFilterAttribute : System.Attribute { }
-}
