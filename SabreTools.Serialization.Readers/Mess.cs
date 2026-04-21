c02698edcda668bbd9d5f70cd4ae2a64df8e2ed1
Matt Nadareski <mnadareski@outlook.com>
2026-04-21 12:22:41 -0400

    Fix M1 and MESS parsing, similar to ListXML

--- SabreTools.Serialization.Readers/Mess.cs
+++ SabreTools.Serialization.Readers/Mess.cs
@@ -897,7 +897,6 @@ namespace SabreTools.Serialization.Readers
                         if (ramOption is not null)
                             ramOptions.Add(ramOption);
 
-                        reader.Skip();
                         break;
 
                     default:
